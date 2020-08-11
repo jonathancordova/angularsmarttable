a754c30d4c64d74d199345a64234420e8462e18b
Jonathan Cordova <jonathandavidcordova@gmail.com>
2020-08-11 15:13:24 -0400

    working version of table

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,40 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'searchfilterexample';
+  settings = {
+    columns: {
+      id: {
+        title: 'ID'
+      },
+      name: {
+        title: 'Full Name'
+      },
+      username: {
+        title: 'User Name'
+      },
+      email: {
+        title: 'Email'
+      }
+    }
+  };
+  data = [
+    {
+      id: 1,
+      name: "test Graham",
+      username: "Bret",
+      email: "test@april.com"
+    },
+    {
+      id: 2,
+      name: "test Howell",
+      username: "test",
+      email: "test@gmail.tv"
+    },
+    {
+      id: 11,
+      name: "testDuBuque",
+      username: "test.Stanton",
+      email: "test@test.biz"
+    }
+  ];
 }
